de18c8fbf941e8f453f6739f56521408d317f00f
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-03-03 17:51:52 +0200

    Update EventBoard.jsx

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -26,7 +26,7 @@ EventBoard.propTypes = {
             location: PropTypes.string.isRequired,
             speaker: PropTypes.string.isRequired,
             type: PropTypes.string.isRequired,
-            time: PropTypes.shape({
+            time: PropTypes.exact({
                 start: PropTypes.string.isRequired,
                 end: PropTypes.string.isRequired
             }),
